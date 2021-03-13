952bcf48a8b94aaba5dabfa644a1c4ecaf98d6a5
rofiikhsan68 <rofii.saputra@gmail.com>
2021-03-13 12:13:58 +0700

    penyewaan

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,6 +9,6 @@ Class Home extends CI_Controller{
     }
     public function index(){
      $this->load->view('home/kontol');
-     $this->load->view('home/kontol');
+     $this->load->view('home/navbar');
     }
 }
