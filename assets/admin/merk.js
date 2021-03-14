40c33dfaefb6881dcdf05e33e33c102f3f32df76
bmhrwn <bmhrwn@gmail.com>
2021-03-15 00:20:29 +0700

    Halaman Utama Customer

--- assets/admin/merk.js
+++ assets/admin/merk.js
@@ -1,16 +1,16 @@
 function add_merk(base_url) {
-	document.getElementById("modal_title").innerHTML = "Form Add Merk";
+	document.getElementById("modal_title").innerHTML = "Form Tambah Merk";
 	document.getElementById("id_merk").value = "";
 	document.getElementById("merk_name").value = "";
-	document.getElementById("button").innerHTML = "Add Data";
+	document.getElementById("button").innerHTML = "Tambah Data";
 	document.getElementById("form").action = base_url;
 }
 function update_merk(base_url, id, name, score) {
-	document.getElementById("modal_title").innerHTML = "Form Update Merk";
+	document.getElementById("modal_title").innerHTML = "Form Ubah Merk";
 	document.getElementById("id_merk").value = id;
 	document.getElementById("merk_name").value = name;
 	document.getElementById("score").value = score;
-	document.getElementById("button").innerHTML = "Update Data";
+	document.getElementById("button").innerHTML = "Ubah Data";
 	document.getElementById("form").action = base_url;
 }
 function delete_merk(base_url) {
