77127195da8d38d345d7a293d84f2e071077eeb7
Farhan Ali Fauzan <farhanalifauzan00@gmail.com>
2021-03-27 01:21:37 +0700

    Revisi Tambah Produk (detik ini)

--- assets/admin/product.js
+++ assets/admin/product.js
@@ -2,6 +2,7 @@ function add_product(base_url){
     document.getElementById("modal_title").innerHTML = "Form Tambah Produk"
     document.getElementById("button").innerHTML = "Tambah Data"
     document.getElementById("product_name").value = ""
+    document.getElementById("price").value = ""
     document.getElementById("merk_name").value = ""
     document.getElementById("type_name").value = ""
     document.getElementById("description").value = ""
@@ -10,10 +11,11 @@ function add_product(base_url){
     document.getElementById("photo").innerHTML = "Choose File"
     document.getElementById("form").action = base_url
 }
-function update_product(base_url,product,merk,type,description,capacity,radius,photo,id){
+function update_product(base_url,product,price,merk,type,description,capacity,radius,photo,id){
     document.getElementById("modal_title").innerHTML = "Form Update Produk"
     document.getElementById("button").innerHTML = "Update Data"
     document.getElementById("product_name").value = product;
+    document.getElementById("price").value = price;
     document.getElementById("merk_name").value = merk;
     document.getElementById("type_name").value = type;
     document.getElementById("description").value = description;
