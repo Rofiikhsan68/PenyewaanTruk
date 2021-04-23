de3238fe1f782a9712df64464cd32f8697b00a5b
gentaprima <gentaprima600@gmail.com>
2021-04-24 01:58:27 +0700

    update

--- application/models/ModelTransaction.php
+++ application/models/ModelTransaction.php
@@ -30,7 +30,7 @@ class ModelTransaction extends CI_Model
             tbl_transaksi.id_cart = tbl_cart.id_cart AND
             tbl_cart.id_user = tbl_user.id_user AND
             tbl_cart.id_product = tbl_product.id_product AND
-            tbl_detailuser.id_user = tbl_user.id_user AND 
+            tbl_detailuser.id_user = tbl_user.id_user AND
             tbl_transaksi.id_transaction = ? GROUP BY id_transaction";
 
         return $this->db->query($sql,$id_transaction)->row_array();
@@ -44,15 +44,34 @@ class ModelTransaction extends CI_Model
         WHERE TR.status = ? GROUP BY TR.id_transaction";
         return $this->db->query($sql,$status)->result_array();
     }
+    public function getDataTransactionByStatusUser($status,$idUser){
+        $sql = "SELECT *,TR.status as status_transaksi FROM tbl_transaksi TR
+        JOIN tbl_cart C ON TR.id_cart = C.id_cart 
+        JOIN tbl_product PR ON C.id_product = PR.id_product 
+        JOIN tbl_user US ON C.id_user = US.id_user 
+        JOIN tbl_detailuser DU ON US.id_user = DU.id_user
+        WHERE TR.status = ? GROUP BY TR.id_transaction AND
+                C.id_user = ?";
+        return $this->db->query($sql,array($status,$idUser))->result_array();
+    }
 
     public function getDataTransactionBy2Status($status1,$status2){
-        $sql = "SELECT * FROM tbl_transaksi TR
+        $sql = "SELECT *,TR.status as status_transaksi FROM tbl_transaksi TR
                     JOIN tbl_cart C ON TR.id_cart = C.id_cart 
                     JOIN tbl_product PR ON C.id_product = PR.id_product 
                     JOIN tbl_user US ON C.id_user = US.id_user 
                     JOIN tbl_detailuser DS ON US.id_user = DS.id_user
                     WHERE TR.status = ? OR TR.status = ? GROUP BY TR.id_transaction ";
         return $this->db->query($sql,array($status1,$status2))->result_array();
+    }
+    public function getDataTransactionBy2StatusUser($status1,$status2,$idUser){
+        $sql = "SELECT *,TR.status as status_transaksi FROM tbl_transaksi TR
+                    JOIN tbl_cart C ON TR.id_cart = C.id_cart 
+                    JOIN tbl_product PR ON C.id_product = PR.id_product 
+                    JOIN tbl_user US ON C.id_user = US.id_user 
+                    JOIN tbl_detailuser DS ON US.id_user = DS.id_user
+                    WHERE TR.status = ? OR TR.status = ? AND C.id_user = ? GROUP BY TR.id_transaction ";
+        return $this->db->query($sql,array($status1,$status2,$idUser))->result_array();
     }
     public function cancelTransactionById($data,$id_transaction){
         return $this->db->update('tbl_transaksi', $data, array('id_transaction' => $id_transaction));
@@ -63,5 +82,18 @@ class ModelTransaction extends CI_Model
 
     public function uploadDp($data,$id_transaction){
         return $this->db->update('tbl_transaksi',$data,['id_transaction' => $id_transaction]);
+    }
+    public function getDataBarangByNumber($number){
+        $sql ="SELECT * FROM tbl_transaksi,tbl_barang
+                WHERE
+                tbl_barang.number_transaction = tbl_transaksi.number AND
+                tbl_barang.number_transaction = ? ";
+                return $this->db->query($sql,$number)->result_array();
+    }
+    public function getNumberByIdTransaksi($id_transaction){
+        return $this->db->get_where('tbl_transaksi', array('id_transaction' => $id_transaction))->row_array();
+    }
+    public function updateStatusByNumber($data,$number){
+        return $this->db->update('tbl_transaksi', $data, array('number' => $number));
     }
 }
